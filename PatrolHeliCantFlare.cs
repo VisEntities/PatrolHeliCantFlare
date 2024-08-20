809a9f33d7b6d1eb486ab5e142f10ba50286110c
VisEntities <visentities@gmail.com>
2024-08-20 10:06:15 +0200

    Added a config option to control the probability that the patrol helicopter is unable to deploy flares. Setting this value to 100 means flares are fully disabled, while lower values allow for a chance of flares being deployed

--- PatrolHeliCantFlare.cs
+++ PatrolHeliCantFlare.cs
@@ -5,20 +5,83 @@
  */
 
 using HarmonyLib;
+using Newtonsoft.Json;
 using Oxide.Core;
 
 namespace Oxide.Plugins
 {
-    [Info("Patrol Heli Cant Flare", "VisEntities", "1.0.0")]
+    [Info("Patrol Heli Cant Flare", "VisEntities", "1.1.0")]
     [Description("Disables the flare functionality for patrol helicopters when targeted by homing missiles.")]
     public class PatrolHeliCantFlare : RustPlugin
     {
         #region Fields
 
         private static PatrolHeliCantFlare _plugin;
+        private static Configuration _config;
         private Harmony _harmony;
 
         #endregion Fields
+
+        #region Configuration
+
+        private class Configuration
+        {
+            [JsonProperty("Version")]
+            public string Version { get; set; }
+
+            [JsonProperty("Percentage Chance That Heli Cannot Use Flares")]
+            public int PercentageChanceThatHeliCannotUseFlares { get; set; }
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            _config = Config.ReadObject<Configuration>();
+
+            if (string.Compare(_config.Version, Version.ToString()) < 0)
+                UpdateConfig();
+
+            SaveConfig();
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            _config = GetDefaultConfig();
+        }
+
+        protected override void SaveConfig()
+        {
+            Config.WriteObject(_config, true);
+        }
+
+        private void UpdateConfig()
+        {
+            PrintWarning("Config changes detected! Updating...");
+
+            Configuration defaultConfig = GetDefaultConfig();
+
+            if (string.Compare(_config.Version, "1.0.0") < 0)
+                _config = defaultConfig;
+
+            if (string.Compare(_config.Version, "1.1.0") < 0)
+            {
+                _config.PercentageChanceThatHeliCannotUseFlares = defaultConfig.PercentageChanceThatHeliCannotUseFlares;
+            }
+
+            PrintWarning("Config update complete! Updated from version " + _config.Version + " to " + Version.ToString());
+            _config.Version = Version.ToString();
+        }
+
+        private Configuration GetDefaultConfig()
+        {
+            return new Configuration
+            {
+                Version = Version.ToString(),
+                PercentageChanceThatHeliCannotUseFlares = 100
+            };
+        }
+
+        #endregion Configuration
 
         #region Oxide Hooks
 
@@ -32,6 +95,7 @@ namespace Oxide.Plugins
         private void Unload()
         {
             _harmony.UnpatchAll(Name + "PATCH");
+            _config = null;
             _plugin = null;
         }
 
@@ -40,10 +104,22 @@ namespace Oxide.Plugins
             if (patrolHelicopter == null)
                 return null;
 
-            return false;
+            if (ChanceSucceeded(_config.PercentageChanceThatHeliCannotUseFlares))
+                return false;
+
+            return null;
         }
 
         #endregion Oxide Hooks
+
+        #region Helper Functions
+
+        private bool ChanceSucceeded(int chance)
+        {
+            return UnityEngine.Random.Range(0, 100) < chance;
+        }
+
+        #endregion Helper Functions
 
         #region Harmony Patches
 
